148f9278de9f84edc2a77f782ce431bec386151f
Peter Gemmell <petergemmell65@hotmail.com>
2020-05-25 13:37:45 +0100

    helper folder and request file created

--- client/whisky_shop/src/App.js
+++ client/whisky_shop/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import ShopContainer from './containers/ShopContainer';
+import WhiskyContainer from './containers/WhiskyContainer';
+import ShoppingCart from './components/cart/ShoppingCart';
+import ShoppingCartOverlay from './components/cart/ShoppingCartOverlay';
+import Dropdown from 'react-overlays/Dropdown';
+import WhiskyList from './components/whiskies/WhiskyList';
+import Whisky from './components/whiskies/Whisky';
+import WhiskyDetail from './components/whiskies/WhiskyDetail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render(){
+    return(
+
+      <div>
+      <ShopContainer />
+      </div>
+    );
+  }
 }
+
 
 export default App;

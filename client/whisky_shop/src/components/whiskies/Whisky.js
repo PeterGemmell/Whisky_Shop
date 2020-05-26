b38a891347d92d7b5abefbc5c123066d4226c2b8
Peter Gemmell <petergemmell65@hotmail.com>
2020-05-26 17:36:15 +0100

    css added to style boxes and product info also added

--- client/whisky_shop/src/components/whiskies/Whisky.js
+++ client/whisky_shop/src/components/whiskies/Whisky.js
@@ -17,11 +17,12 @@ class Whisky extends Component {
       <DistilleryMap distillery={this.props.whisky.distilleryName} latitude={this.props.whisky.latitude} longitude={this.props.whisky.longitude} />
       <div className="info">
       <p><b><u>{this.props.whisky.distilleryName}</u></b></p>
-      <p><i>Name </i>{this.props.whisky.productName}</p>
-      <p><i>Region </i>{this.props.whisky.region}</p>
-      <p><i>Strength </i>{this.props.whisky.strength}</p>
-      <p><i>Volume </i>{this.props.whisky.volume}</p>
+      <p><b>Name </b>{this.props.whisky.productName}</p>
+      <p><b>Region </b>{this.props.whisky.region}</p>
+      <p><b>Strength </b>{this.props.whisky.strength}</p>
+      <p><b>Volume </b>{this.props.whisky.volume}</p>
       <p> £ {this.props.whisky.retailPrice} </p>
+      <p><b>Product Info</b></p>
       <p> {this.props.whisky.productInfo} </p>
       </div>
       </div>

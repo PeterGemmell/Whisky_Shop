43dd4abd38ede565bd2280ff107641f2f4f98cbf
Peter Gemmell <petergemmell65@hotmail.com>
2020-05-28 16:51:52 +0100

    age container and form created for age check

---
+++ client/whisky_shop/src/containers/AgeContainer.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import AgeForm from '../components/age/AgeForm.js';
+
+
+class AgeContainer extends Component {
+
+  constructor(props) {
+  super(props);
+
+  this.date = new Date()
+  this.year = this.date.getFullYear()
+  this.month = this.date.getMonth()
+  this.years = Array.from(Array(100), (x, index) => index + (this.date.getFullYear() - 99)).reverse()
+
+  this.state = {
+    display: "form",
+    userMonth: 0,
+    userYear: 2000,
+    isChecked: false
+  };
+}
+
+componentDidMount() {
+  let alreadyChecked = localStorage.getItem('checkedObj')
+
+  if (alreadyChecked) this.setState({display: "welcome"})
+}
+
+handleSubmit = (e) => {
+  e.preventDefault()
+  if (this.state.userYear < this.year - 21 || (this.state.userYear === this.year - 21 && this.state.userMonth <= this.month)) {
+    if (this.state.isChecked) {
+      let checkedObj = {'alreadyChecked': true}
+      localStorage.setItem('checkedObj', JSON.stringify(checkedObj))
+    }
+    this.setState({display: "welcome"})
+  } else {
+    this.setState({display: "failure"})
+  }
+}
+
+handleNotOfAgeClick = () => {
+  this.setState({display: "failure"})
+}
+
+handleMonthChange = (e) => {
+  this.setState({userMonth: parseInt(e.target.value)})
+}
+
+handleYearChange = (e) => {
+  this.setState({userYear: parseInt(e.target.value)})
+}
+
+toggleCheck = () => {
+  this.setState({isChecked: !this.state.isChecked})
+}
+
+render () {
+  const options = this.years.map(year => {
+    return <option key={year} value={year}>{year}</option>
+  })
+  const display = this.state.display
+
+  if (display === "form") {
+    return (
+      <AgeForm
+        options={options}
+        userMonth={this.state.userMonth}
+        userYear={this.state.userYear}
+        isChecked={this.state.isChecked}
+        handleSubmit={this.handleSubmit}
+        handleMonthChange={this.handleMonthChange}
+        handleYearChange={this.handleYearChange}
+        handleNotOfAgeClick={this.handleNotOfAgeClick}
+        toggleCheck={this.toggleCheck}
+        />
+    )
+   }
+  }
+};
+
+
+
+
+export default AgeContainer;
--- client/whisky_shop/src/containers/ShopContainer.js
+++ client/whisky_shop/src/containers/ShopContainer.js
@@ -4,6 +4,7 @@ import NavBar from '../NavBar.js'
 import WhiskyContainer from './WhiskyContainer';
 import LoginContainer from './LoginContainer';
 import AboutContainer from './AboutContainer';
+import AgeContainer from './AgeContainer'; //remove if wrong
 
 
 const ShopContainer = () => {
@@ -12,6 +13,7 @@ const ShopContainer = () => {
       <Fragment>
       <NavBar/>
       <Switch>
+      <Route path="/age" component={AgeContainer} /> //remove if wrong.
       <Route path="/login" component={LoginContainer} />
       <Route path="/whiskies" component={WhiskyContainer} />
       <Route path="/about" component={AboutContainer} />
